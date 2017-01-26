6a01f38b348dab30eefc4b595f3c99989b3eb21d
partouf <partouf@gmail.com>
2017-01-26 20:31:26 +0100

    write errors to the console output and set the exitcode

--- GitBitter/Program.cs
+++ GitBitter/Program.cs
@@ -1,25 +1,32 @@
 ﻿namespace GitBitter
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
     using GitBitterLib;
 
     class Program
     {
         static void Main(string[] args)
         {
-            if (args.Length > 0)
+            try
             {
-                var unwrapper = new PackageUnwrapper(args[0]);
-                unwrapper.StartAndWaitForUnwrapping();
+                if (args.Length > 0)
+                {
+                    var unwrapper = new PackageUnwrapper(args[0]);
+                    unwrapper.StartAndWaitForUnwrapping();
+                }
+                else
+                {
+                    var unwrapper = new PackageUnwrapper();
+                    unwrapper.StartAndWaitForUnwrapping();
+                }
+
+                Environment.Exit(0);
             }
-            else
+            catch (Exception ex)
             {
-                var unwrapper = new PackageUnwrapper();
-                unwrapper.StartAndWaitForUnwrapping();
+                Console.WriteLine(ex.Message + "\n\n" + (ex.InnerException != null ? ex.InnerException.Message + "\n\n" + ex.InnerException.StackTrace : ex.StackTrace));
+
+                Environment.Exit(ex.HResult);
             }
         }
     }
