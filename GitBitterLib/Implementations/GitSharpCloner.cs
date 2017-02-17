82f590c0d88bee17835e2eb24f22f62765dd1b6c
partouf <partouf@gmail.com>
2017-02-17 20:38:44 +0100

    spaces

--- GitBitterLib/Implementations/GitSharpCloner.cs
+++ GitBitterLib/Implementations/GitSharpCloner.cs
@@ -1,121 +1,121 @@
 ﻿namespace GitBitterLib
 {
-	using System;
-	using System.IO;
-	using System.Threading.Tasks;
-	using LibGit2Sharp;
-	using LibGit2Sharp.Handlers;
-	using Microsoft.Practices.Unity;
+    using System;
+    using System.IO;
+    using System.Threading.Tasks;
+    using LibGit2Sharp;
+    using LibGit2Sharp.Handlers;
+    using Microsoft.Practices.Unity;
 
-	public class GitSharpCloner : ICloner
-	{
-		private const string AppNameBitbucket = "gitbitter:bitbucket";
-		private const string AppNameGithub = "gitbitter:github";
-		private Identity identity;
-		private bool useSSH = false;
+    public class GitSharpCloner : ICloner
+    {
+        private const string AppNameBitbucket = "gitbitter:bitbucket";
+        private const string AppNameGithub = "gitbitter:github";
+        private Identity identity;
+        private bool useSSH = false;
 
-		public GitSharpCloner()
-		{
-			LoadSettings();
-		}
+        public GitSharpCloner()
+        {
+            LoadSettings();
+        }
 
-		private void LoadSettings()
-		{
-			var gitConfig = new GitConfig();
+        private void LoadSettings()
+        {
+            var gitConfig = new GitConfig();
 
-			identity = new Identity(gitConfig.UserName, gitConfig.UserEmail);
-			useSSH = gitConfig.UseSSH;
-		}
+            identity = new Identity(gitConfig.UserName, gitConfig.UserEmail);
+            useSSH = gitConfig.UseSSH;
+        }
 
-		private string GetUrlForRepository(string repository)
-		{
-			if (useSSH)
-			{
+        private string GetUrlForRepository(string repository)
+        {
+            if (useSSH)
+            {
 #if MONO
-				// todo: find out why we need to do this on OSX/mono
-				return repository.Replace("https://", "git://git@");
+                // todo: find out why we need to do this on OSX/mono
+                return repository.Replace("https://", "git://git@");
 #else
 				return repository.Replace("https://", "ssh://git@");
 #endif
-			}
-			else
-			{
-				return repository;
-			}
-		}
+            }
+            else
+            {
+                return repository;
+            }
+        }
 
-		public Task Clone(string repository, string rootdir, string repodir, string branch)
-		{
-			var task = new Task(() =>
-			{
-				var logging = GitBitterContainer.Default.Resolve<IGitBitterLogging>();
+        public Task Clone(string repository, string rootdir, string repodir, string branch)
+        {
+            var task = new Task(() =>
+            {
+                var logging = GitBitterContainer.Default.Resolve<IGitBitterLogging>();
 
-				string url = GetUrlForRepository(repository);
+                string url = GetUrlForRepository(repository);
 
-				string stage = "initialization";
-				try
-				{
-					var options = GetCloneOptions(url, branch);
-					var fullRepoPath = Path.Combine(rootdir, repodir);
+                string stage = "initialization";
+                try
+                {
+                    var options = GetCloneOptions(url, branch);
+                    var fullRepoPath = Path.Combine(rootdir, repodir);
 
-					stage = "cloning";
-					Repository.Clone(url, fullRepoPath, options);
+                    stage = "cloning";
+                    Repository.Clone(url, fullRepoPath, options);
 
-					logging.Add("Finished cloning " + repodir, LoggingLevel.Info);
-				}
-				catch (Exception ex)
-				{
-					throw new ClonerException(ex, repodir, url, stage);
-				}
-			});
-			task.Start();
-			return task;
+                    logging.Add("Finished cloning " + repodir, LoggingLevel.Info);
+                }
+                catch (Exception ex)
+                {
+                    throw new ClonerException(ex, repodir, url, stage);
+                }
+            });
+            task.Start();
+            return task;
         }
 
         public Task ResetAndUpdateExisting(string repository, string rootdir, string repodir, string branch)
         {
             var task = new Task(() =>
             {
-				var logging = GitBitterContainer.Default.Resolve<IGitBitterLogging>();
+                var logging = GitBitterContainer.Default.Resolve<IGitBitterLogging>();
 
-				string url = GetUrlForRepository(repository);
+                string url = GetUrlForRepository(repository);
 
-				string stage = "initialization";
-				try
-				{
-					var fullRepoPath = Path.Combine(rootdir, repodir);
+                string stage = "initialization";
+                try
+                {
+                    var fullRepoPath = Path.Combine(rootdir, repodir);
 
-					var options = new PullOptions();
-					options.FetchOptions = new FetchOptions();
+                    var options = new PullOptions();
+                    options.FetchOptions = new FetchOptions();
 
-					if (useSSH)
-					{
-						options.FetchOptions.CredentialsProvider = GetCredentialHandlerSSH(repository);
-					}
-					else
-					{
-						options.FetchOptions.CredentialsProvider = GetCredentialHandler(repository);
-					}
+                    if (useSSH)
+                    {
+                        options.FetchOptions.CredentialsProvider = GetCredentialHandlerSSH(repository);
+                    }
+                    else
+                    {
+                        options.FetchOptions.CredentialsProvider = GetCredentialHandler(repository);
+                    }
 
-					var sig = new Signature(identity, DateTime.Now);
+                    var sig = new Signature(identity, DateTime.Now);
 
-					var repo = new Repository(fullRepoPath);
+                    var repo = new Repository(fullRepoPath);
 
-					stage = "reset";
-					repo.Reset(ResetMode.Hard);
+                    stage = "reset";
+                    repo.Reset(ResetMode.Hard);
 
-					stage = "pull";
-					Commands.Pull(repo, sig, options);
+                    stage = "pull";
+                    Commands.Pull(repo, sig, options);
 
-					stage = "checkout " + branch;
-					Commands.Checkout(repo, branch);
+                    stage = "checkout " + branch;
+                    Commands.Checkout(repo, branch);
 
-					logging.Add("Finished updating " + repodir, LoggingLevel.Info);
-				}
-				catch (Exception ex)
-				{
-					throw new ClonerException(ex, repodir, url, stage);
-				}
+                    logging.Add("Finished updating " + repodir, LoggingLevel.Info);
+                }
+                catch (Exception ex)
+                {
+                    throw new ClonerException(ex, repodir, url, stage);
+                }
             });
             task.Start();
             return task;
--- GitBitterLib/Interfaces/IGitBitterLogging.cs
+++ GitBitterLib/Interfaces/IGitBitterLogging.cs
@@ -1,15 +1,16 @@
 ﻿namespace GitBitterLib
 {
-	using System;
+    using System;
 
-	public enum LoggingLevel {
-		Error = 1,
-		Info = 2,
-		Debugging = 3
-	};
+    public enum LoggingLevel
+    {
+        Error = 1,
+        Info = 2,
+        Debugging = 3
+    };
 
-	public interface IGitBitterLogging
-	{
-		void Add(string AMessage, LoggingLevel ALevel);
-	}
+    public interface IGitBitterLogging
+    {
+        void Add(string AMessage, LoggingLevel ALevel);
+    }
 }
